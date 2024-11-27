846e3f93c7d737a4c3035f25199603c8ba210e1d
fefefe14021 <145283892+fefefe14021@users.noreply.github.com>
2024-11-27 21:07:07 +0500

    tasks.h

---
+++ лаба 5/tasks.h
@@ -0,0 +1,14 @@
+#ifndef TASKS_H
+#define TASKS_H
+
+#include <string>
+
+// Объявление функций, решающих различные задачи
+void task1(const std::string& filename);  // Задание 1: Подсчёт удвоенных нечётных чисел
+void task2(const std::string& filename, int m, int n);  // Задание 2: Преобразование матрицы
+void task3(const std::string& filename);  // Задание 3: Поиск самой дешёвой игрушки
+void task4(const std::string& filename);  // Задание 4: Квадрат разности максимального и минимального чисел
+void task5(const std::string& filename);  // Задание 5: Сумма нечётных элементов
+void task6(const std::string& inputFile, const std::string& outputFile);  // Задание 6: Последний символ каждой строки
+
+#endif // TASKS_H
